32ce3527e796d66e72d00f22d0d68dfd46208a07
cjmccann <connor.j.mccann@gmail.com>
2014-02-24 00:47:29 -0500

    Added img.onload because of asynchronicity

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -11,20 +11,22 @@ function draw () {
 	ctx.fillRect (0, 0, 800, 600);
 
 	// Load sprite sheet
-	var oImg=document.createElement("img");
-	oImg.setAttribute('src', 'assets/duckhunt.png');
-	oImg.setAttribute('alt', 'na');
+	var imgSheet=document.createElement("img");
+	imgSheet.setAttribute('src', 'assets/duckhunt.png');
+	imgSheet.setAttribute('alt', 'na');
 
-	// Drawing images on screen
-	// Order of drawing: tree, road, dog, birdx5
-	//					 sx,  sy,  sw,  sh,  dx,  dy,  dw,  dh
-	ctx.drawImage(oImg,   0, 271,  72, 126,  40, 260, 144, 252);
-	ctx.drawImage(oImg,   0, 714, 900, 182,   0, 418, 900, 182); 
-	ctx.drawImage(oImg,   0,   0,  62,  46, 200, 440, 186, 138);
-	ctx.drawImage(oImg,   0, 116,  35,  35, 400,  45,  70,  70);
-	ctx.drawImage(oImg, 125, 117,  39,  33, 200, 150,  78,  66);
-	ctx.drawImage(oImg, 296, 152,  40,  40, 600, 300,  80,  80);
-	ctx.drawImage(oImg,  39, 236,  30,  37, 350, 270,  60,  74);
-	ctx.drawImage(oImg,  76, 152,  40,  40, 620,  60,  80,  80);
+	imgSheet.onload = function() {
+		// Drawing images on screen
+		// Order of drawing: tree, road, dog, birdx5
+		//					 sx,  sy,  sw,  sh,  dx,  dy,  dw,  dh
+		ctx.drawImage(imgSheet,   0, 271,  72, 126,  40, 260, 144, 252);
+		ctx.drawImage(imgSheet,   0, 714, 900, 182,   0, 418, 900, 182); 
+		ctx.drawImage(imgSheet,   0,   0,  62,  46, 200, 440, 186, 138);
+		ctx.drawImage(imgSheet,   0, 116,  35,  35, 400,  45,  70,  70);
+		ctx.drawImage(imgSheet, 125, 117,  39,  33, 200, 150,  78,  66);
+		ctx.drawImage(imgSheet, 296, 152,  40,  40, 600, 300,  80,  80);
+		ctx.drawImage(imgSheet,  39, 236,  30,  37, 350, 270,  60,  74);
+		ctx.drawImage(imgSheet,  76, 152,  40,  40, 620,  60,  80,  80);
+	};
 }
 
